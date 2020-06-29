319b09ed94539d68688485568d6ccec8768220d4
hygn <proton_hygn@protonmail.com>
2020-06-30 00:20:10 +0900

    commit NCO module

---
+++ LUTGEN.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sys
+i = 0
+ibws1 = str(int(sys.argv[3])-1)
+obws1 = str(int(sys.argv[2]) - 1)
+ibw = str(int(sys.argv[3]))
+obw = sys.argv[2]
+code = "module lut ( input en, input["+ibws1+":0] lookup, output["+obws1+":0] out);\n" 
+code = code + "always @(en)begin\n"
+code = code + "    case (lookup)\n"
+while i <= int(sys.argv[1]):
+    if i == 0 :
+        samp = 0
+    else:
+        samp = i/int(sys.argv[1]) * 2 * np.pi
+    val = int(round(np.sin(samp)*(pow(2,int(sys.argv[2])-1)-1)+pow(2,int(sys.argv[2])-1)-1))
+    code = code + "        "+ibw+"'d"+str(i)+": out <= "+obw+"'d"+str(val)+";\n"
+    i = i + 1
+code = code + "    endcase\n"
+code = code + "end\n"
+code = code + "endmodule"
+print(code)
---
+++ NCOdrive.v
@@ -0,0 +1,19 @@
+module ncotop(
+    input clk,
+    output b0,b1,b2,b3,b4,b5,b6,b7,update,truncate,
+);
+reg[7:0] sine;
+reg[15:0] cosine;
+reg[9:0] accadr;
+nco u1 (32'b00000001000000000000000000000000,1,clk,sine,cosine,update,truncate);
+assign b0 = sine[0];
+assign b1 = sine[1];
+assign b2 = sine[2];
+assign b3 = sine[3];
+assign b4 = sine[4];
+assign b5 = sine[5];
+assign b6 = sine[6];
+assign b7 = sine[7];
+assign update = update;
+assign truncate = truncate;
+endmodule // ncotop
