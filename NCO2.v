140729f17edb587ec2baa28cab5326b296ea742a
hygn <proton_hygn@protonmail.com>
2020-07-01 00:26:56 +0900

    rewrite NCO core

---
+++ NCO2.v
@@ -0,0 +1,24 @@
+module nco (
+    input [lutsize:0] Iftw,
+    input [7:0] Fftw,
+    input clk,
+    input en,
+    output[7:0] sine,
+);
+// FTW[lutsize+7:8] = integer
+// FTW[7:0] = fraction
+reg[lutsize+7:0] addr;
+reg[lutsize+7:0] ftw;
+reg[lutsize:0] lookup;
+parameter lutsize = 10;
+always @(posedge clk && en) begin
+    ftw[lutsize+7:8] <= Iftw;
+    ftw[7:0] <= Fftw;
+    addr <= addr+ftw;
+    if (addr[7] == 1'b1) begin
+        addr[lutsize+7:8] <= addr[lutsize+7:8] + 1;
+    end
+    lookup <= addr[lutsize+7:8];
+end
+lut u0 (clk,lookup,sine);
+endmodule
--- NCOdrive.v
+++ NCOdrive.v
@@ -1,11 +1,10 @@
 module ncotop(
     input clk,
-    output b0,b1,b2,b3,b4,b5,b6,b7,update,truncate,
+    output b0,b1,b2,b3,b4,b5,b6,b7,
 );
 reg[7:0] sine;
-reg[15:0] cosine;
-reg[9:0] accadr;
-nco u1 (32'b00000001000000000000000000000000,1,clk,sine,cosine,update,truncate);
+//nco u1 (32'b00000001000000000000000000000000,1,clk,sine,cosine,update,truncate);
+nco u1 (11'd32,8'd250,clk,1,sine);
 assign b0 = sine[0];
 assign b1 = sine[1];
 assign b2 = sine[2];
@@ -14,6 +13,6 @@ assign b4 = sine[4];
 assign b5 = sine[5];
 assign b6 = sine[6];
 assign b7 = sine[7];
-assign update = update;
-assign truncate = truncate;
+//assign update = update;
+//assign truncate = truncate;
 endmodule // ncotop
